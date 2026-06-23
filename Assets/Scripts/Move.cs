d84824ae2c9a682269a3561b406f57a95913ca37
hmj2622010Onoe <hmj2622010@stu.o-hara.ac.jp>
2026-06-23 15:10:32 +0900

    プレイヤーが移動できるように更新

--- Assets/Scripts/Move.cs
+++ Assets/Scripts/Move.cs
@@ -6,8 +6,7 @@ public class Move : MonoBehaviour
 	Rigidbody2D rigid2D;
 	[SerializeField]float walkForce = 30.0f;
 	[SerializeField]float jumpForce = 400.0f;
-	float maxWalkSpeed = 2.0f;
-	bool groundFlag = false;
+	float maxWalkSpeed = 3.0f;
 	
 	// Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
@@ -19,28 +18,19 @@ public class Move : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-		if(Keyboard.current.aKey.IsPressed() && rigid2D.linearVelocityX < maxWalkSpeed) 
-		{
-			rigid2D.AddForce(-transform.right * walkForce);
-		}
 		if(Keyboard.current.dKey.IsPressed() && rigid2D.linearVelocityX < maxWalkSpeed) 
 		{
 			rigid2D.AddForce(transform.right * walkForce);
 		}
+		if(Keyboard.current.aKey.IsPressed() && rigid2D.linearVelocityX > -maxWalkSpeed) 
+		{
+			rigid2D.AddForce(-transform.right * walkForce);
+		}
 
-		if (Keyboard.current.wKey.wasPressedThisFrame && groundFlag == true) 
+		if (Keyboard.current.wKey.wasPressedThisFrame) 
 		{
 			rigid2D.AddForce(transform.up * jumpForce);
 		}
 		
-	}
-	private void OnTriggerStay2D(Collider2D collision)
-	{
-		groundFlag = true;
-	}
-
-	private void OnTriggerExit2D(Collider2D collision)
-	{
-		groundFlag = false;
 	}
 }
